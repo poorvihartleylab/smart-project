3773a6f0a8c4bbfcfdfa81888157546ea986f24d
Hartley Lab <hello@hartleylab.com>
2018-09-10 17:50:22 +0530

    create view file

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Registration Form</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user-regis', function () {
+    return view('test');
+});
